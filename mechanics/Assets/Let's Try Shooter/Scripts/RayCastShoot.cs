2fbd24d4858452249faf167295c7fa7d72a88714
Tom Layzell <tom.layzell@hotmail.com>
2017-09-07 18:32:56 +0100

    commit1

---
+++ mechanics/Assets/Let's Try Shooter/Scripts/RayCastShoot.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastShoot : MonoBehaviour {
+
+    public int GunDamage = 1;
+    public float FireRate = .25f;
+    public float WeaponRange = 50f;
+    public float HitForce = 100f;
+    public Transform gunEnd;
+
+    private Camera fpsCam;
+    private WaitForSeconds shotDuration = new WaitForSeconds( .07f);
+    private AudioSource gunAudio;
+    private LineRenderer laserLine;
+    private float nextFire;
+
+	// Use this for initialization
+	void Start () {
+        laserLine = GetComponent<LineRenderer>();
+        gunAudio = GetComponent<AudioSource>();
+        fpsCam = GetComponentInParent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown ("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + FireRate;
+
+            StartCoroutine(ShotEffect());
+
+            Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3 (0.5f, 0.5f, 0));
+            RaycastHit hit;
+
+            laserLine.SetPosition(0, gunEnd.position);
+
+            if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit, WeaponRange))
+            {
+                laserLine.SetPosition(1, hit.point);
+
+                ShootableBox health = hit.collider.GetComponent<ShootableBox>();
+
+                if (health != null)
+                {
+                    health.Damage(GunDamage);
+                }
+
+                if (hit.rigidbody != null)
+                {
+                    hit.rigidbody.AddForce( -hit.normal * HitForce);
+                }
+            }
+            else
+            {
+                laserLine.SetPosition(1, rayOrigin + (fpsCam.transform.forward * WeaponRange));
+            }
+        }
+	}
+
+    private IEnumerator ShotEffect ()
+    {
+        gunAudio.Play();
+
+        laserLine.enabled = true;
+        yield return shotDuration;
+        laserLine.enabled = false;
+    }
+}
